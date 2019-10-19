9b958a4bffdf152114a796800661e4d1b278a24c
Paweł Tomasik <pawel.tomasik@pictec.eu>
2019-10-19 13:24:38 +0200

    fwks submodule

--- template_files/runtime.sh
+++ template_files/runtime.sh
@@ -6,4 +6,9 @@ export JUPYTER_CONFIG_DIR=/root/.jupyter
 apt update
 apt install -y git
 apt install -y sox
+
+pushd /asr/fwks_submod
+git pull
+popd
+
 pip install git+https://github.com/detly/gammatone.git
