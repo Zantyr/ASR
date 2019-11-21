559c0bbc2ef89ec9030f9056efd8e88cdecebded
Paweł Tomasik <pawel.tomasik@pictec.eu>
2019-11-21 10:25:50 +0100

    Last snapshot

--- dissertation_model/__init__.py
+++ dissertation_model/__init__.py
@@ -1,27 +1,3 @@
-"""
-Handcrafted + MFCC
-CZT model
-~~~~~
-Tasks:
-- DenoisingTask
-- FeatureLearnabilityTask
-Other:
-- PER metric
-- WER metric
-- add Language models to abstract training
-
-> implement models
-> implement "mock" option
-> installer
-
-> add Language models everywhere
-> prepare dataset download scripts
-
-> FeatureLearnabilityTask
-> Task.mock
-"""
-
-
 import fwks.model as model_mod
 import fwks.noise_gen
 import fwks.stage as stage
@@ -30,7 +6,9 @@ import fwks.miscellanea as miscellanea
 from fwks.stage import RandomSelectionAdapter
 
 import keras
+import keras.backend as K
 import numpy as np
+import tensorflow as tf
 
 
 def mfcc_model(preprocessing_distortion=None):
@@ -195,7 +173,7 @@ def HandcraftedMFCC():
     ])
 
 
-def CZTModel():
+def czt_model():
     return model_mod.AcousticModel([
         stage.Window(512, 128),
         stage.CZT(z=0.999, w=0.9995 * np.exp(2j * np.pi / 512)),
@@ -210,12 +188,10 @@ def CZTModel():
         miscellanea.StopOnConvergence(4),
     ])
 
-##################################################################
-
-
 def stft_max_melfbank_model():
     return model_mod.AcousticModel([
-        stage.WindowedTimeFrequencyFBank(preset="stft", summary="max"),
+        stage.WindowedTimeFrequencyFBank(),
+        stage.LogPower(),
         stage.MelFilterbank(64),
         stage.MeanStdNormalizer(),
         stage.CNN2D(channels=16, filter_size=5, depth=2),
@@ -226,14 +202,13 @@ def stft_max_melfbank_model():
         keras.callbacks.TerminateOnNaN(),
         miscellanea.StopOnConvergence(4),
     ])
-
-
+    
 def learnable_filters_model():
     return model_mod.AcousticModel([
         stage.Window(512, 128),
-        ########################
-        stage.LearnableFilterbank(),
-        ########################
+        stage.PlainPowerFourier(),
+        stage.LearnableFourierFBanks(64),
+        # stage.DCT(24),
         stage.CNN2D(channels=16, filter_size=5, depth=2),
         stage.RNN(width=512, depth=2),
         stage.phonemic_map(37),
@@ -242,14 +217,11 @@ def learnable_filters_model():
         keras.callbacks.TerminateOnNaN(),
         miscellanea.StopOnConvergence(4),
     ])
-
 
 def missing_fundamental_model():
     return model_mod.AcousticModel([
-        stage.Window(512, 128),
-        ###
-        ###
-        stage.MelFilterbank(64),
+        stage.CustomCochleagram(64, 512, 128),
+        stage.LogPower(),
         stage.MeanStdNormalizer(),
         stage.CNN2D(channels=16, filter_size=5, depth=2),
         stage.RNN(width=512, depth=2),
@@ -260,19 +232,66 @@ def missing_fundamental_model():
         miscellanea.StopOnConvergence(4),
     ])
 
+def separated_phonemes_model_factory(phone_list):
+    def maker():
+        mapping = [
+            ["a", 'o', 'e', 'I', 'i', 'u', 'j', 'w', 'sil'],
+            ["tsi", 'ts', 'dz', 'dZ', 'tS', 'dzi', 'S', 'zi', 's', 'z', 'Z', 'si'],
+            ['p', 't', 'k', 'b', 'd', 'g', 'r', 'l', 'v', 'f', 'x'],
+            ['n', 'on', 'en', 'm', 'ni']
+        ]
 
-def separated_phonemes_model():
-    return model_mod.AcousticModel([
-        stage.Window(512, 128),
-        stage.MelFilterbank(64),
-        stage.MeanStdNormalizer(),
-        ###
-        stage.CNN2D(channels=16, filter_size=5, depth=2),
-        stage.RNN(width=512, depth=2),
-        stage.phonemic_map(37),
-        ###
-        stage.CTCLoss(selection_adapter=RandomSelectionAdapter())
-    ], callbacks=[
-        keras.callbacks.TerminateOnNaN(),
-        miscellanea.StopOnConvergence(4),
-    ])
+        def mapping_fn(model, columns):
+            print(model.symbol_map, mapping)
+            sel = [(
+                ([ix for ix, col in enumerate(mapping) if x in col][0]),
+                ([col for ix, col in enumerate(mapping) if x in col][0].index(x))
+            ) for x in model.symbol_map]
+            print(sel)
+            sel = [columns[x][:, :, y] for (x, y) in sel]
+            return K.stack(sel, axis=-1)
+
+        def make_short_mapping(howmuch):
+            def inner(inp):
+                return keras.layers.Dense(howmuch)(inp)
+            return inner
+
+        def make_snd_mapping(inp):
+            return keras.layers.Lambda(lambda x:
+                K.softmax(
+                    tf.pad(x, [(0, 0), (0, 0), (0, 1)], constant_values=1)
+                )
+            )(inp)
+
+        concat = stage.Columns([
+                [
+                    stage.CNN2D(channels=4, filter_size=5, depth=2),
+                    stage.RNN(width=256, depth=2),
+                    stage.CustomNeural(
+                        make_short_mapping(len(x))
+                    )
+                ]
+                for x in mapping
+            ], mapping=mapping_fn)
+
+        mdl = model_mod.AcousticModel([
+            stage.Window(512, 128),
+            stage.LogPowerFourier(),
+            stage.MelFilterbank(64),
+            stage.DCT(24),
+            stage.MeanStdNormalizer(),
+            concat,
+            stage.CustomNeural(
+                make_snd_mapping
+            ),
+            stage.CTCLoss(selection_adapter=RandomSelectionAdapter())
+        ], callbacks=[
+            keras.callbacks.TerminateOnNaN(),
+            miscellanea.StopOnConvergence(4),
+        ])
+
+        concat.model_ref = mdl
+        mdl.symbol_map = phone_list
+
+        return mdl
+    return maker
